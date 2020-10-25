86b6ee1c93cc2983fce8b39a7d103fdbb7443bf1
Aidar Chakiev <achakiev2003@gmail.com>
2020-10-25 20:27:27 +0600

    6 fix

--- 6/L6-Problem-4: Elements on odd.py
+++ 6/L6-Problem-4: Elements on odd.py
@@ -3,11 +3,12 @@ b = a.split(",")
 list1 = [int(x) for x in b]
 odd = []
 
-for i in range(len(list1)):
-    if list1[i] % 2 == 0:
-        pass
-    else:
-        odd.append(list1[i])
+odd.append(list1[0])
+for i in range(1, len(list1)):
 
+    if i % 2 == 0:
+        odd.append(list1[i])
+    else:
+        pass
 
 print(odd)
